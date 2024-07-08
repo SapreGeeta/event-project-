64e5178d68f821a9501471153d5d1a2480abba66
SapreGeeta <gitasapre@gmail.com>
2024-07-08 21:24:09 +0530

    last commit

--- backend/index.js
+++ backend/index.js
@@ -19,6 +19,14 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.get("/",(req,res)=>{
+  res.json({
+    success:true,
+    message:"successful",
+  });
+});
+
+
 app.use("/api/v1/message", messageRouter);
 
 mongoose
